31c9ddf9ef1a585a082c938fd8607b9e2507a092
Mike Cooper <mikescooper@icloud.com>
2019-09-23 04:01:49 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  
+  attr_accessor :side1, :side2, :side3 
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end 
+  
+  def kind 
+    is_triangle?
+    if side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 == side2 || side2 == side3 || side3 == side1
+      :isosceles
+    elsif side1 != side2 && side2 != side3 && side3 != side1
+      :scalene
+    end 
+  end 
+  
+  def is_triangle?
+    all_sides = [side1,side2,side3]
+    
+    if all_sides.reduce(:+) == 0 || all_sides.any?{|side| side < 0 } 
+      raise TriangleError
+    elsif side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2 
+      raise TriangleError
+    end 
+  end 
+  
+  class TriangleError < StandardError 
+  end 
+  
+  
+  
+  
 end
