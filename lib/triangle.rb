60ff1720e5780662fe1a1ca7f34412aa36117635
Mike Cooper <mikescooper@icloud.com>
2019-09-23 03:54:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,17 +11,22 @@ class Triangle
   end 
   
   def kind 
-    all_sides = [side1,side2,side3]
-    
     if side1 == side2 && side2 == side3
       :equilateral
     elsif side1 == side2 || side2 == side3 || side3 == side1
       :isosceles
     elsif side1 != side2 && side2 != side3 && side3 != side1
       :scalene
-    elsif all_sides.reduce == 0 || all_sides.any?{|side| side < 0} 
+  end 
+  
+  def is_triangle?
+    all_sides = [side1,side2,side3]
+    
+    if 
+      all_sides.reduce == 0 || all_sides.any?{|side| side < 0 }
       raise TriangleError
     end 
+    
   end 
   
   class TriangleError < StandardError 
