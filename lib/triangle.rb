7e967d154e50e2b9ad2e2786d36e2f48c34a11dd
Mike Cooper <mikescooper@icloud.com>
2019-09-23 04:01:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -26,7 +26,7 @@ class Triangle
     
     if all_sides.reduce(:+) == 0 || all_sides.any?{|side| side < 0 } 
       raise TriangleError
-    elsif side1 + side2 < side3 || side2 + side3 < side1 || side1 + side3 < side2 
+    elsif side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2 
       raise TriangleError
     end 
   end 
